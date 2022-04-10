3ee2362700d8b6d4c91468fa905f4a5c63e60465
Einar Rosvoll <pigzelner@gmail.com>
2022-04-10 12:25:35 +0200

    Fix borrow issues

--- src/parse.rs
+++ src/parse.rs
@@ -1,4 +1,4 @@
-struct ParseNode {
+pub struct ParseNode {
 	id: Option<String>,
 	title: String,
 	properties: std::collections::HashMap<String, String>,
@@ -7,7 +7,7 @@ struct ParseNode {
 pub fn parse_module(path: &str) -> Vec<ParseNode> {
 	let mut parse_nodes: Vec<ParseNode> = Vec::new();
 
-	let text = std::fs::read_to_string(path).expect(format!("Could not read {path}").as_str());
+	let text = std::fs::read_to_string(path).expect(format!("Could not read {}", path).as_str());
 	for line in text.split("\n") {
 		if line.starts_with("# ") {
 			let mut splitter = line.splitn(3, " ");
@@ -15,7 +15,7 @@ pub fn parse_module(path: &str) -> Vec<ParseNode> {
 			
 			parse_nodes.push(ParseNode {
 				title: splitter.next().unwrap().to_string(),
-				id: splitter.next().or(Some("".to_string())),
+				id: splitter.next().map(|s| s.to_string()),
 				properties: std::collections::HashMap::new(),
 			});
 		} else if line.trim().len() > 0 {
@@ -25,10 +25,10 @@ pub fn parse_module(path: &str) -> Vec<ParseNode> {
 
 			p.properties.insert(
 				key.to_string(),
-				splitter.next().or(Some("".to_string())).unwrap(),
+				splitter.next().unwrap_or("").to_string(),
 			);
 
-			println!("{}({}): {}={}", p.title, p.id.or(Some("".to_string())).unwrap(), key, p.properties.get(key).unwrap());
+			println!("{}({}): {}={}", p.title, p.id.as_ref().unwrap_or(&"".to_string()), key, p.properties.get(key).unwrap());
 		}
 	}
 
