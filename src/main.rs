04b40f09a469b9187b4d43465d0b65e6c552d2e2
Einar Rosvoll <pigzelner@gmail.com>
2022-04-24 14:08:46 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,6 @@ mod nodes;
 mod parse;
 mod parse_nodes;
 
-//use crate::nodes::sine::{SineNode};
-//use crate::nodes::{Node, NodeData};
-
-//use crate::audio_output;
-
-
 fn main() {
 	parse::parse_module("example_project/main.txt");
 	//process::test_process();
