07958d7e6a0eaf92b7ae5c3907172a0ef9161e6c
Einar Rosvoll <einar.rosvoll@gmail.com>
2024-08-03 14:02:22 +0200

    Only init and commit to git if no .git folder

--- aim/init_folder.py
+++ aim/init_folder.py
@@ -7,8 +7,8 @@ def init_folder(path: str) -> bool:
 	import os, sys
 	path = os.path.abspath(path)
 
-	if os.listdir(path):
-		print(f"Folder is not empty.", file=sys.stderr)
+	if collision_files := {"main.py", ".data", ".local"} & set(os.listdir(path)):
+		print(f"Folder is not empty: {' '.join(collision_files)}", file=sys.stderr)
 		return False
 
 	os.mkdir(path + os.path.sep + ".data")
@@ -20,13 +20,18 @@ def init_folder(path: str) -> bool:
 	with open(".data/version", "w") as f:
 		f.write("1")
 
-
 	with open(".gitignore", "w") as f:
 		f.write(".local/")
 
 	import subprocess
-	subprocess.run(["git", "init", "-q"])
+	git_already_inited = ".git" not in os.listdir()
+
+	if not git_already_inited:
+		subprocess.run(["git", "init", "-q"])
+
 	subprocess.run(["git", "add", ".data", ".gitignore", "main.py"])
-	subprocess.run(["git", "commit", "-qm", "Aim project created"])
+
+	if not git_already_inited:
+		subprocess.run(["git", "commit", "-qm", "Aim project created"])
 
 	return True
